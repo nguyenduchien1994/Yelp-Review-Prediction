5108cb8a76c09a2a3822e17334c6548816fd6d4c
jsburke <jsburke@bu.edu>
2017-08-04 16:58:13 -0400

    update cleanse for a long run

--- scratch_jsburke/cleanse.py
+++ scratch_jsburke/cleanse.py
@@ -16,21 +16,27 @@ with open("../yelp_academic_dataset_business.json") as business_json:
 	for line in business_json:
 		business = json.loads(line)
 		if business['categories'] is not None:
+			if 'Food' in business['categories']:
+				business_info.append({'business_id': business['business_id'], 'rating': business['stars']})
+				ids.append(business['business_id'])
 			if 'Restaurants' in business['categories']:
 				business_info.append({'business_id': business['business_id'], 'rating': business['stars']})
 				ids.append(business['business_id'])
-			elif 'Food' in business['categories']:
-				business_info.append({'business_id': business['business_id'], 'rating': business['stars']})
-				ids.append(business['business_id'])
 
+print('tea found')
+
+count = 0
 review_info = []
 with open('../yelp_academic_dataset_review.json') as review_json:
     for line in review_json:
+    	count += 1
         review = json.loads(line)
         if review['business_id'] in ids:
             review_info.append({'business_id': review['business_id'],
                                 'review': review['text'].replace('?', '')
                                 .replace(',', '').replace('.', '')})
+
+print('reviews found : %d'%len(review_info))
 
 business_info = pd.DataFrame(business_info)
 
@@ -42,15 +48,19 @@ info = pd.merge(business_info, review_info, how='inner', on='business_id')
 
 info.head()
 
-vectorizer = TfidfVectorizer(stop_words='english', min_df=0.1, max_df=0.8,
-                             max_features=1000, ngram_range=(1,2))
-dtm = vectorizer.fit_transform(info.review.values).toarray()
+info.to_csv('others.csv', encoding='utf-8')
 
-X = dtm
-le = preprocessing.LabelEncoder()
-y = le.fit_transform(info.rating.values)
-train_set_size = 19979
-X_train = X[:train_set_size]
-y_train = y[:train_set_size]
-X_test = X[train_set_size:]
-y_test = y[train_set_size:]
+print("complete coffee and tea")
+
+# vectorizer = TfidfVectorizer(stop_words='english', min_df=0.1, max_df=0.8,
+#                              max_features=1000, ngram_range=(1,2))
+# dtm = vectorizer.fit_transform(info.review.values).toarray()
+
+# X = dtm
+# le = preprocessing.LabelEncoder()
+# y = le.fit_transform(info.rating.values)
+# train_set_size = 19979
+# X_train = X[:train_set_size]
+# y_train = y[:train_set_size]
+# X_test = X[train_set_size:]
+# y_test = y[train_set_size:]
