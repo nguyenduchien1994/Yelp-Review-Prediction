35a8673f21266ba0efd5cd4d4726fc2a86ff1533
jsburke <jsburke@bu.edu>
2017-08-03 21:50:19 -0400

    include in not None check

--- scratch_jsburke/cleanse.py
+++ scratch_jsburke/cleanse.py
@@ -14,11 +14,19 @@ rest_ctr = 0
 with open("../yelp_academic_dataset_business.json") as business_json:
 	for line in business_json:
 		business = json.loads(line)
-		if 'Restaurants' in business['categories']:
-			# print('Restaurants' in business['categories'])
-			print(True)
-		else:
-			continue
+		if business['categories'] is not None:
+			if 'Restaurants' in business['categories']:
+				bus_app(business)
+			elif 'Food' in business['categories']:
+				bus_app(business)
+
+		# thoughts meanderings, super tired, to be with john
+
+		# if 'Restaurants' in business['categories']:
+		# 	# print('Restaurants' in business['categories'])
+		# 	print(True)
+		# else:
+		# 	continue
 
 		#if business['stars'] >= 3.6:
 		#	last_good = business
