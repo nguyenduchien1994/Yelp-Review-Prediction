ca56b670a32342825cb8d4076fbb6dda5a985438
jsburke <jsburke@bu.edu>
2017-08-12 12:09:18 -0400

    small script to run multiple iterations of python

---
+++ final/RUNME.pl
@@ -0,0 +1,21 @@
+: # -*- perl -*-
+  eval 'exec perl -S  $0 ${1+"$@"}' 
+    if 0;  # if running under some shell
+#
+
+system("clear");
+
+$script       = "./predict.py";
+@reviewCounts = (500, 5000, 10000, 15000, 25000, 50000);
+$iters        = 10;
+
+print "Running Yelp Review Predictions\n";
+
+foreach $count(@reviewCounts){
+	print "For $count reviews\n";
+	for( $i = 0; $i < $iters; $i = $i + 1){
+		system("$script -r $count");
+	}
+}
+
+print "Script Complete\n";
